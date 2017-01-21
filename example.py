6b28e3a0765b10a9f9cdf917559aa7bb8499dbfd
Artem Rizhov <artem.rizhov@gmail.com>
2017-01-22 01:08:17 +0400

    Change logging so that the messages appear once on thread status change
    Closes #21.

--- example.py
+++ example.py
@@ -8,11 +8,19 @@ def sleep(t):
 if __name__ == '__main__':
     print("Starting the deadlocks monitoring")
     monitoring_thread = start_monitoring(seconds_frozen=1)
+    print("Sleep 3 seconds in custom func")
     sleep(3)
-    time.sleep(2)
+    print("Sleep 3 seconds")
+    time.sleep(3)
+    print("Sleep 3 seconds")
+    time.sleep(3)
+
     print("Stopping the deadlocks monitoring")
     # This may be useful when working in shell.
     monitoring_thread.stop()
+    print("Sleep 3 seconds")
+    time.sleep(3)
+    print("Sleep 3 seconds")
     time.sleep(3)
 
     print("Exiting")
